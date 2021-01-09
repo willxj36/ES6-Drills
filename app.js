7200655da09455018626efcaf588b8d1073b09af
willxj36 <willxj36@gmail.com>
2021-01-08 20:00:02 -0700

    done with arrow functions

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-function faveMovie(movie = 'The Room', name = 'Tommy Wiseau') {
-    console.log(`My name is ${name} and my favorite movie is ${movie}!`);
-}
+faveMovie = (movie = 'The Room', name = 'Tommy Wiseau') => console.log(`My name is ${name} and my favorite movie is ${movie}!`);
 
 faveMovie();
 faveMovie('Snatch');
 faveMovie('Snatch', 'Will Seese');
+
+getFirstName = (name) => {
+    let firstName = (name.split(' '))[0];
+    console.log(firstName);
+}
+getFirstNameConcise = (name) => console.log((name.split(' '))[0]);
+
+getFirstName('Will Seese');
+getFirstNameConcise('Will Seese');
+
+doMath = (x, y) => ({exponent: x**y, product: x*y});
+console.log(`${doMath.exponent}, ${doMath.product}`);
